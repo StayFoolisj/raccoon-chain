1b9c7e9b7bd99b6ce393a8b70d8cc1b5be9fb165
StayFoolisj <hav.muggerud@gmail.com>
2020-11-13 13:21:29 +0800

    added the issuance of staking tokens + test coverage

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,6 +5,8 @@ import "./DaiToken.sol";
 
 contract TokenFarm {
     string public name = "Raccoon Chain Token Farm";
+    address public owner;
+
     DappToken public dappToken;
     DaiToken public daiToken;
 
@@ -18,10 +20,13 @@ contract TokenFarm {
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // 1. Stakes Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        // Require amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
 
         // Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
@@ -43,6 +48,18 @@ contract TokenFarm {
 
 
     // Issuing Tokens
+    function issueTokens() public {
+        // Only owner can call this function
+        require(msg.sender == owner, "caller must be the owner");
 
+        // Issue tokens to all stakers
+        for (uint i=0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
 }
 
