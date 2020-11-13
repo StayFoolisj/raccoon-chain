946f7a282dbdf77b81495acb844f2b28d5ede168
StayFoolisj <hav.muggerud@gmail.com>
2020-11-13 13:06:50 +0800

    wrote functionality for depositing tokens for staking + added test coverage

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,6 +7,13 @@ contract TokenFarm {
     string public name = "Raccoon Chain Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+
+    // Keeping track of all stakers
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
     
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
@@ -15,10 +22,27 @@ contract TokenFarm {
 
     // 1. Stakes Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add user to stakers array ONLY if they haven't staked already
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 
-    // 2. Unstaking Tokens (Withdraw)
+    // Unstaking Tokens (Withdraw)
 
-    // 3. Issuing Tokens
+
+    // Issuing Tokens
+
 }
 
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,6 +8,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+// ETH doesnt operate with decimals so we use this function to make the digits human friendly
 function tokens(n) {
     return web3.utils.toWei(n, 'ether');
 }
@@ -52,6 +53,33 @@ contract('TokenFarm', ([owner, investor]) => {
         it('contract has tokens', async () => {
             let balance = await dappToken.balanceOf(tokenFarm.address)
             assert.equal(balance.toString(), tokens('1000000'))
+        })
+    })
+
+    describe('Farming tokens', async () => {
+        it('rewards investors for staking mDai tokens', async () => {
+            let result
+
+            // Check investor balance before staking
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'))
+
+            // Stake Mock DAI Tokens
+            await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor } )
+            await tokenFarm.stakeTokens(tokens('100'), { from: investor} )
+
+            // Check staking result
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor Mock DAI wallet balance correct after staking')
+            
+            result = await daiToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), tokens('100'), 'Token Farm Mock DAI balance correct after staking')
+        
+            result = await tokenFarm.stakingBalance(investor)
+            assert.equal(result.toString(), tokens('100'), 'Investor staking bnalance correct after staking')
+
+            result = await tokenFarm.isStaking(investor)
+            assert.equal(result.toString(), 'true', 'investor staking status correct after staking')
         })
     })
 })
