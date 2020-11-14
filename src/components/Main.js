46c7e144da91ec27a9c384dea46e67af76099156
StayFoolisj <hav.muggerud@gmail.com>
2020-11-14 22:44:14 +0800

    improved upon the UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import dai from '../dai.png'
+import dai from "../dai.png";
 
 class Main extends Component {
   render() {
@@ -29,57 +29,64 @@ class Main extends Component {
           </tbody>
         </table>
         <div className="card mb-4">
-          <div className="card-body"></div>
-          <form
-            className="mb-3"
-            onSubmit={(event) => {
-              event.preventDefault();
-              let amount;
-              amount = this.input.value.toString();
-              amount = window.web3.utils.toWei(amount, "Ether");
-              this.props.stakeTokens(amount);
-            }}
-          >
-            <div>
-              <label className="float-left">
-                <b>Stake Tokens</b>
-              </label>
-              <span className="float-right text-muted">
-                Balance:{" "}
-                {window.web3.utils.fromWei(this.props.daiTokenBalance, "Ether")}
-              </span>
-            </div>
-            <div className="input-group mb-4">
-              <input
-                type="text"
-                ref={(input) => {
-                  this.input = input;
-                }}
-                className="form-control form-control-lg"
-                placeholder="0"
-                required
-              />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <img src={dai} height="32" alt="" />
-                  &nbsp;&nbsp;&nbsp; mDAI
+          <div className="card-body">
+            <form
+              className="mb-3"
+              onSubmit={(event) => {
+                event.preventDefault();
+                let amount;
+                amount = this.input.value.toString();
+                amount = window.web3.utils.toWei(amount, "Ether");
+                this.props.stakeTokens(amount);
+              }}
+            >
+              <div>
+                <label className="float-left">
+                  <b>Stake Tokens</b>
+                </label>
+                <span className="float-right text-muted">
+                  Balance:{" "}
+                  {window.web3.utils.fromWei(
+                    this.props.daiTokenBalance,
+                    "Ether"
+                  )}
+                </span>
+              </div>
+              <div className="input-group mb-4">
+                <input
+                  type="text"
+                  ref={(input) => {
+                    this.input = input;
+                  }}
+                  className="form-control form-control-lg"
+                  placeholder="0"
+                  required
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <img src={dai} height="32" alt="" />
+                    &nbsp;&nbsp;&nbsp; mDAI
+                  </div>
                 </div>
               </div>
-            </div>
-            <button type="submit" className="btn btn-primary btn-block btn-lg">
-              STAKE!
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-lg"
+              >
+                STAKE!
+              </button>
+            </form>
+            <button
+              type="submit"
+              className="btn btn-link btn-block btn-sm"
+              onClick={(event) => {
+                event.preventDefault();
+                this.props.unstakeTokens();
+              }}
+            >
+              UN-STAKE...
             </button>
-          </form>
-          <button
-            type="submit"
-            className="btn btn-link btn-block btn-sm"
-            onClick={(event) => {
-              event.preventDefault();
-              this.props.unstakeTokens();
-            }}
-          >
-            UN-STAKE...
-          </button>
+          </div>
         </div>
       </div>
     );
